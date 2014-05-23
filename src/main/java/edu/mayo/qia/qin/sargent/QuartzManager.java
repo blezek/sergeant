12ef747194600d3ebf0043ef47e27af3ff3be234
Daniel Blezek <daniel.blezek@gmail.com>
2014-05-23 06:35:15 -0500

    Quartz support

---
+++ src/main/java/edu/mayo/qia/qin/sargent/QuartzManager.java
@@ -0,0 +1,26 @@
+package edu.mayo.qia.qin.sargent;
+
+import io.dropwizard.lifecycle.Managed;
+
+import org.quartz.Scheduler;
+
+public class QuartzManager implements Managed {
+
+  public Scheduler scheduler;
+
+  public QuartzManager(Scheduler scheduler) {
+    this.scheduler = scheduler;
+  }
+
+  @Override
+  public void start() throws Exception {
+    scheduler.start();
+
+  }
+
+  @Override
+  public void stop() throws Exception {
+    scheduler.shutdown(true);
+  }
+
+}
